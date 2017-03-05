d8379217c68891e31d9199e755133ddd7c64a81d
cshah <cshah@tavisca.com>
2017-03-05 23:04:21 +0530

    Initial Commit

---
+++ GcdApplicaltion/GcdOperationManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcdApplicaltion
+{
+    class GcdOperationManager
+    {
+
+        public int GCDLogic(int fristNumber, int secondNumber)
+        {
+            int Remainder;
+
+            while (secondNumber != 0)
+            {
+                Remainder = fristNumber % secondNumber;
+                fristNumber = secondNumber;
+                secondNumber = Remainder;
+            }
+
+            return fristNumber;
+        }
+    }
+}
---
+++ GcdApplicaltion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcdApplicaltion
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int fristNumber, secondNumber;
+            Console.Write("Enter the First Number : ");
+            fristNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Second Number : ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            GcdOperationManager gcdOperationManagerObject = new GcdOperationManager();
+            Console.Write("\nThe Greatest Common Divisor of ");
+            Console.WriteLine("{0} and {1} is {2}", fristNumber, secondNumber, gcdOperationManagerObject.GCDLogic(fristNumber, secondNumber));
+            Console.WriteLine("Thankyou\nPress any key to exit the application");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
+
